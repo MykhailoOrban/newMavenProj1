83584e191c06a974a4db2bb14a140106afd3af17
Mykhailo Orban <mikhail.orban@gmail.com>
2024-01-14 19:59:16 +0200

    HomeWork

---
+++ src/main/java/ex/Main.java
@@ -0,0 +1,13 @@
+package ex;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class Main {
+    public static void main(String[] args) throws JsonProcessingException {
+        User user = new User("Mykhailo", "Orban");
+        ObjectMapper om = new ObjectMapper();
+        System.out.println(om.writeValueAsString(user));
+
+    }
+}
---
+++ src/main/java/ex/User.java
@@ -0,0 +1,11 @@
+package ex;
+
+public class User {
+    public String name;
+    public String lastName;
+
+    public User(String name, String surname){
+        this.name= name;
+        this.lastName = surname;
+    }
+}
